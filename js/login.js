774f0e7f17adc8b6ec33f1c002cbe8b33ac4a58b
zach georgii <zgeorgii@gmail.com>
2016-02-22 13:30:35 -0800

    started login

---
+++ js/login.js
@@ -0,0 +1,8 @@
+var ref = new Firebase("https://you-choose.firebaseio.com");
+ref.authWithOAuthPopup("google", function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
