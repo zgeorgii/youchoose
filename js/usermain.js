f6d7dda93fb43593543aea21c272191b8fc75ed0
zach georgii <zgeorgii@gmail.com>
2016-02-22 14:36:49 -0800

    added google login w/firebase

---
+++ js/usermain.js
@@ -0,0 +1,6 @@
+// angular.module('rtfmApp')
+//   .controller('mainPageController', function($scope, $location, userService, threadService){
+//     $scope.user = userService.getLoggedInUser();
+//
+//     $scope.newThreadTitle
+//   })
