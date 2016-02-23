8e2fbc7c7873d3e32c0df1172142c6ab784aa6ca
zach georgii <zgeorgii@gmail.com>
2016-02-23 09:51:04 -0800

    file setup change and login started

---
+++ app.js
@@ -0,0 +1,19 @@
+angular.module('rtfmApp', ['ngRoute', 'firebase'])
+.constant('fb', {
+  url: 'https://you-choose.firebaseio.com'
+})
+.config(function($routeProvider){
+  $routeProvider.
+      when('/login', {
+          templateUrl: 'pages/login/login.html'
+      }).
+      when('/main', {
+          templateUrl: 'pages/main/main.html'
+      }).
+      when('/thread/:threadId', {
+          templateUrl: 'pages/thread/thread.html'
+      }).
+      otherwise({
+          redirectTo: '/login'
+      });
+});
