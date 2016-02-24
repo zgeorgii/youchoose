5db1765073fa9ea73bf3c95f333ce5d93c383184
chadob <chadnobrowne@gmail.com>
2016-02-24 11:28:11 -0800

    made for elements black fixed search function wrote some block authors function to store object properties in an array

--- js/filterAuthors.js
+++ js/filterAuthors.js
@@ -1,3 +1,11 @@
+function grabBlockedAuthors (user) {
+  var blockedAuthors = [];
+  for (var blockedAuthors in user) {
+    blockedAuthors.push user[blockedAuthors];
+  }
+  return blockedAuthors;
+};
+
 var filteredOutUsers = users.blockedAuthors.forEach(function(ele) {
   videosByCategory.all.filter(function(vid) {
     return vid.snippet.channelTitle !== ele;
