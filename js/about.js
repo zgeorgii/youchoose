7daa9f1f95059d174f403c817023ebda49ce63fd
zach georgii <zgeorgii@gmail.com>
2016-02-25 21:51:26 -0800

    route to about

---
+++ js/about.js
@@ -0,0 +1,11 @@
+(function(module) {
+  var aboutController = {};
+
+  aboutController.index = function() {
+    $('header').hide();
+    $('.videos').hide();
+    $('settings-page').hide();
+    $('.about-page').show();
+  };
+  module.aboutController = aboutController;
+})(window);
