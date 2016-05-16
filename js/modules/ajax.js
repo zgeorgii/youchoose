a67a3dd11066b1ee126b0808e8d9d7a9dc272589
zach georgii <zgeorgii@gmail.com>
2016-05-16 14:19:31 -0700

    finally making this look good

---
+++ js/modules/ajax.js
@@ -0,0 +1,37 @@
+(function ($) {
+  if ($(window).width() <= 1024) {
+    console.log("mobile");
+   $.ajax({
+     url     : 'vendors/jquery.mobile.custom.min.js',
+     dataType : 'script'
+   }).done(function() {
+      $.ajax({
+        url      : 'js/controllers/blockedController.js',
+        dataType : 'script'
+      }).done(function() {
+        $.ajax({
+          url      : 'js/viewportSpecific/mobileFunctionality.js',
+          dataType : 'script'
+        });
+      });
+    });
+  } else {
+   $.ajax({
+     url      : 'vendors/jquery-ui-1.11.4.custom/jquery-ui.min.js',
+     dataType : 'script'
+   }).done(function() {
+     $.ajax({
+       url      : 'js/controllers/blockedController.js',
+       dataType : 'script',
+       succes: function() {
+       }
+     }).done(function() {
+       $.ajax({
+         url      : 'js/viewportSpecific/requestVideosDesktop.js',
+         dataType : 'script'
+       });
+     });
+   });
+   console.log("pc");
+ };
+})(jQuery);
